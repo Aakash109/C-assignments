cddc951846fdd97c5490c6ca66f06fa12aa16f71
Aakash109 <98142600+Aakash109@users.noreply.github.com>
2022-02-22 23:09:43 +0530

    swap_by_value

---
+++ swap_by_value.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void swap(int &a, int &b){
+	int temp = a;
+	a = b;
+	b = temp;
+	cout<<"After swap: a:="<<a<<" b :="<<b<<endl;
+}
+
+int main(){	
+	int a = 2;
+	int b = 5;
+	cout<<"before swap: a:="<<a<<" b :="<<b<<endl;
+	cout<<"swap using value"<<endl;
+	swap(a,b);
+	return 0;
+}
