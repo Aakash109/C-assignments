fc29588868f8e384818fe4dd7a7a1ceff144e7c0
Aakash109 <98142600+Aakash109@users.noreply.github.com>
2022-02-23 12:29:23 +0530

    operatoroverloading_unary_2

---
+++ operatoroverloading_unary_2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+class space{
+  int x,y,z;
+  public:
+  space(){
+      x=y=z=0;}
+  space(int a,int b,int c){
+      x=a;
+      y=b;
+      z=c;
+  }
+  void operator --();
+  void displ(){
+      cout<<"\nx="<<x<<"\ny="<<y<<"\nz="<<z<<endl;}
+  };
+void space::operator --(){
+    x--;
+    y--;z--;
+}
+    int main()
+{
+    space s1(5,3,2);
+    s1.displ();
+    --s1;
+    s1.displ();
+    return 0;
+}
