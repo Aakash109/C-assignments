2036d0f2d582fd686178c1e3f5f0ce581e69892f
Aakash109 <98142600+Aakash109@users.noreply.github.com>
2022-03-19 10:38:56 +0530

    exception

---
+++ exception_handling.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+void Xtest(int test){
+cout<<"Inside Xtest,test is "<<test<<"\n";
+if(test) throw test;
+}
+
+int main(){
+
+cout<<"Start\n";
+try{
+cout<<"Inside try block\n";
+Xtest(0);
+Xtest(1);
+Xtest(2);
+}
+
+catch(int i){
+cout<<"caught an exception -- value is ";
+cout<<i<<"\n";
+}
+cout<<"end";
+return 0;
+}
+
