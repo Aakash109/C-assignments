b21f40a73cfb94422f944239e95a47bb46848286
Aakash109 <98142600+Aakash109@users.noreply.github.com>
2022-03-19 10:39:54 +0530

    e.what()_code

---
+++ e.what()_code.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<exception>
+using namespace std;
+int main(){
+try{
+int *myarray=new int [100000000000000];
+}
+catch(exception &e){
+cout<<"std exception"<<e.what()<<endl;
+}
+return 0;
+}
